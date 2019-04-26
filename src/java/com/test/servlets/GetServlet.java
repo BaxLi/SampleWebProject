d205abc76ac4de3b6d184f817af5465a6d8c39a6
BaxLi <lilian.baxan@gmail.com>
2019-04-26 12:37:58 +0300

    Step+1

--- src/java/com/test/servlets/GetServlet.java
+++ src/java/com/test/servlets/GetServlet.java
@@ -5,7 +5,9 @@
  */
 package com.test.servlets;
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,11 +16,14 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author User
  */
+@WebServlet("/getServlet")
 public class GetServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp); //To change body of generated methods, choose Tools | Templates.
+        String htmlResponse="<html><h2>Servlet resp example</h2></html>";
+        PrintWriter writer=resp.getWriter();
+        writer.write(htmlResponse);
     }
-    
 }
